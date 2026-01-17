171970b7b03c2d78ee5e6b09289a4749d647fd60
nonydev27 <karldjansi123@gmail.com>
2026-01-17 23:38:35 +0000

    styling page

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,23 +1,30 @@
 export default function Signup() {
   return (
     <>
-      <div className="p-5 bg-blue-300">
-        {/*  Declaring inputs in sections*/}
-        {/* Section 1(First && Surname) */}
-        <div className="col col-2">
-          <input type="text" name="firstname" id="" placeholder="Firstname" />
-          <input type="text" name="surname" id="" placeholder="Surname" />
-        </div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="p-10 bg-blue-300 rounded-2xl">
+          {/*  Declaring inputs in sections*/}
+          {/* Section 1(First && Surname) */}
+          <div className="col col-2">
+            <input type="text" name="firstname" id="" placeholder="Firstname" />
+            <input type="text" name="surname" id="" placeholder="Surname" />
+          </div>
 
-        {/* Section 2(Email && Password) */}
-        <div className="col col-2">
-          <input type="text" name="email" id="" placeholder="Email" />
-          <input type="password" name="password" id="" placeholder="password" />
-        </div>
+          {/* Section 2(Email && Password) */}
+          <div className="col col-2">
+            <input type="text" name="email" id="" placeholder="Email" />
+            <input
+              type="password"
+              name="password"
+              id=""
+              placeholder="password"
+            />
+          </div>
 
-        {/* Section 3(Contact && DOB)*/}
-        <input type="text" name="" id="" placeholder="Contact" />
-        <input type="date" name="" id="" placeholder="dob" />
+          {/* Section 3(Contact && DOB)*/}
+          <input type="text" name="" id="" placeholder="Contact" />
+          <input type="date" name="" id="" placeholder="dob" />
+        </div>
       </div>
     </>
   );
