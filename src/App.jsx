b8198bf5c25a0db37c41c62875ecc4c84a0e830c
nonydev27 <karldjansi123@gmail.com>
2026-01-17 23:09:19 +0000

    adding inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
     <>
-      <div className="">
+      <div className="h-4 w-2 bg-amber-500 ">
+        {/*  Declaring inputs*/}
         <div></div>
       </div>
     </>
