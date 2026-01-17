0de45b4f4713dcb04cc639755ded84a8a8cbf088
nonydev27 <karldjansi123@gmail.com>
2026-01-17 23:25:16 +0000

    added more attributes to input

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -5,13 +5,14 @@ export default function Signup() {
         {/*  Declaring inputs in sections*/}
         {/* Section 1(First && Surname) */}
         <div>
-          <input type="text" placeholder="Firstname" />
-          <input type="text" placeholder="Surname" />
+          <input type="text" name="firstname" id="" placeholder="Firstname" />
+          <input type="text" name="surname" id="" placeholder="Surname" />
         </div>
 
-        {/* Section 2() */}
+        {/* Section 2(Email && Password) */}
         <div>
-          <input type="text" />
+          <input type="text" name="email" id="" placeholder="Email" />
+          <input type="password" name="password" id="" placeholder="password" />
         </div>
       </div>
     </>
