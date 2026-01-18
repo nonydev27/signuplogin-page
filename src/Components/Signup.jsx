705697dcc2722b3103ea497a267e930e7e17b477
nonydev27 <karldjansi123@gmail.com>
2026-01-18 11:06:17 +0000

    added more fields

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -4,13 +4,16 @@ export default function Signup() {
       <div className="flex justify-center items-center h-screen">
         <div className="p-10 bg-blue-300 rounded-2xl">
           {/*  Declaring inputs in sections*/}
-          {/* Section 1(First && Surname) */}
-          <div className="col col-2">
+          {/* Section 1(Firstname */}
+          <div className="col">
             <input type="text" name="firstname" id="" placeholder="Firstname" />
+          </div>
+          {/* Section 2(Surname) */}
+          <div className="col">
             <input type="text" name="surname" id="" placeholder="Surname" />
           </div>
 
-          {/* Section 2(Email && Password) */}
+          {/* Section 3(Email && Password) */}
           <div className="col col-2">
             <input type="text" name="email" id="" placeholder="Email" />
             <input
@@ -21,7 +24,7 @@ export default function Signup() {
             />
           </div>
 
-          {/* Section 3(Contact && DOB)*/}
+          {/* Section 4(Contact && DOB)*/}
           <div className="col col-2">
             <input type="text" name="" id="" placeholder="Contact" />
             <input type="date" name="" id="" placeholder="dob" />
