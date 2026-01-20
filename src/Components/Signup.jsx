b8f0dbff82d9a06b4e482f0e8d98463cfbc1b981
nonydev27 <karldjansi123@gmail.com>
2026-01-20 07:49:03 +0000

    got rid of some div elements

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,7 +1,7 @@
 export default function Signup() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center items-center h-screen shadow-2xl">
         <div className="p-10 bg-blue-300 rounded-3xl">
           {/*  Declaring inputs in sections*/}
           {/* Section 1(Firstname */}
@@ -9,25 +9,23 @@ export default function Signup() {
             <input type="text" name="firstname" id="" placeholder="Firstname" />
           </div>
           {/* Section 2(Surname) */}
-          <div className="col p-4">
-            <input type="text" name="surname" id="" placeholder="Surname" />
-          </div>
+
+          <input type="text" name="surname" id="" placeholder="Surname" />
 
           {/* Section 3(Email && Password) */}
-          <div className="col col-2 p-4">
-            <input type="text" name="email" id="" placeholder="Email" />
-            <input
-              type="password"
-              name="password"
-              id=""
-              placeholder="password"
-            />
-          </div>
+
+          <input type="text" name="email" id="" placeholbder="Email" />
+          <input type="password" name="password" id="" placeholder="password" />
 
           {/* Section 4(Contact && DOB)*/}
+
+          <input type="text" name="" id="" placeholder="Contact" />
+          <input type="date" name="" id="" placeholder="dob" />
+
           <div className="col col-2 p-4">
-            <input type="text" name="" id="" placeholder="Contact" />
-            <input type="date" name="" id="" placeholder="dob" />
+            <button name="submit" type="submit">
+              Sign Up
+            </button>
           </div>
         </div>
       </div>
